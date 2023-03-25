7681ea820eb1aa5be54764601e0d9a50cebf970d
Eric Philippot <EricpDev@gmail.com>
2023-03-25 01:26:32 -0500

    Learned safe user input handling and best methods

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const path = require("path");
 
+const people = [
+    {
+        id: 0,
+        name: "Homer Simpson"
+    },
+    {
+        id: 1,
+        name: "Marge Simpson",
+    },
+    {
+        id: 2,
+        name: "Bart Simpson",
+    },
+    {
+        id: 3,
+        name: "Lisa Simpson",
+    },
+    {
+        id: 4,
+        name: "Maggie Simpson"
+    }
+];
+
 const PORT = 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello!! :)");
+});
+
+app.get("/people", (req, res) => res.json(people));
+
+app.get("/people/:personID", (req, res) => {
+    const personID = Number(req.params.personID);
+    const person = people[personID];
+    if(person) {
+        res.status(200).json(person);
+    }else {
+        res.status(404).json({
+            error: "Person does not exist..."
+        });
+    }
 });
 
 app.get("/messages", (req, res) => {
