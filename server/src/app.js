899e98522178026a09d3040f6afb00f7e6ebd535
Eric Philippot <EricpDev@gmail.com>
2023-04-11 22:25:32 -0500

    added planets route, controller, model

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const planetsRouter = require("./routes/planets/planets.router");
 
 
 const app = express();
@@ -7,7 +9,11 @@ const app = express();
 
 //======================================//
 //========= Express Middleware =========//
+app.use(cors({
+    origin: "http://localhost:3000",
+})); // Returns cors middleware;
 app.use(express.json());
+app.use(planetsRouter);
 
 
 
--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,18 @@ const http = require("http");
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
-
 const server = http.createServer(app);
 
+
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+    console.log(
+        `
+       ****************************************
+            Node Dev Project:
+            \x1b[95m Site: http://localhost:${PORT}/ \x1b[0m
+        **************************************
+
+            Listening on port ${PORT}...
+        `
+    );
 })
