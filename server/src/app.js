b78b2cb6daeed5ebdf034b6032ca8c1b9cd43802
Eric Philippot <EricpDev@gmail.com>
2023-04-14 01:04:59 -0500

    added build and deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 
 
@@ -13,7 +14,11 @@ app.use(cors({
     origin: "http://localhost:3000",
 })); // Returns cors middleware;
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 
 
